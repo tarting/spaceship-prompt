162548715beabf24f652e4bf129ed884a647f858
Trygvi Bech Arting <trygvi@gmail.com>
2020-10-30 13:17:21 +0100

    my config

--- spaceship.zsh
+++ spaceship.zsh
@@ -43,35 +43,35 @@ if [ -z "$SPACESHIP_PROMPT_ORDER" ]; then
     time          # Time stampts section
     user          # Username section
     dir           # Current directory section
-    host          # Hostname section
     git           # Git section (git_branch + git_status)
-    hg            # Mercurial section (hg_branch  + hg_status)
+#    hg            # Mercurial section (hg_branch  + hg_status)
     package       # Package version
-    node          # Node.js section
-    ruby          # Ruby section
-    elm           # Elm section
-    elixir        # Elixir section
-    xcode         # Xcode section
-    swift         # Swift section
+#    node          # Node.js section
+#    ruby          # Ruby section
+#    elm           # Elm section
+#    elixir        # Elixir section
+#    xcode         # Xcode section
+#    swift         # Swift section
     golang        # Go section
-    php           # PHP section
+#    php           # PHP section
     rust          # Rust section
     haskell       # Haskell Stack section
     julia         # Julia section
     docker        # Docker section
-    aws           # Amazon Web Services section
-    gcloud        # Google Cloud Platform section
+#    aws           # Amazon Web Services section
+#    gcloud        # Google Cloud Platform section
     venv          # virtualenv section
     conda         # conda virtualenv section
     pyenv         # Pyenv section
-    dotnet        # .NET section
-    ember         # Ember.js section
-    kubectl       # Kubectl context section
-    terraform     # Terraform workspace section
+#    dotnet        # .NET section
+#    ember         # Ember.js section
+#    kubectl       # Kubectl context section
+#    terraform     # Terraform workspace section
     exec_time     # Execution time
     line_sep      # Line break
-    battery       # Battery level and status
-    vi_mode       # Vi-mode indicator
+    host          # Hostname section
+#    battery       # Battery level and status
+#    vi_mode       # Vi-mode indicator
     jobs          # Background jobs indicator
     exit_code     # Exit code section
     char          # Prompt character
